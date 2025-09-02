3eee299435a66e87bbf6c3d64d57c289bdb112d8
Ruherpan <148195357+OhSeungHyun12@users.noreply.github.com>
2025-09-02 21:16:43 +0900

    Add files via upload

---
+++ ros2_ws/src/yolo_orb3_ros2/launch/yolo_orb3_rviz2.launch.py
@@ -0,0 +1,56 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    # yolo_orb3_ros2 pakage dir
+    yolo_orb3_ros2_share_dir = get_package_share_directory('yolo_orb3_ros2')
+
+    camera_node = Node(
+        package='v4l2_camera',
+        executable='v4l2_camera_node',
+        name='camera',
+        output='screen'
+    )
+
+    # Limit FPS: 15FPS
+    image_throttle_node = Node(
+        package='topic_tools',
+        executable='throttle',
+        name='image_throttle',
+        arguments=['messages', '/image_raw', '15', '/image_raw_low'],
+        output='screen'
+    )
+
+    # Running OLO + ORB-SLAM
+    yolo_orb_slam_node = Node(
+        package='yolo_orb3_ros2',
+        executable='mono_ar',
+        name='yolo_orb_slam',
+        output='screen',
+        arguments=[
+            os.path.join(yolo_orb3_ros2_share_dir, 'orb_slam3', 'Vocabulary', 'ORBvoc.txt'),
+            os.path.join(yolo_orb3_ros2_share_dir, 'orb_slam3', 'Examples', 'ROS2', 'webcam.yaml')
+        ],
+        remappings=[
+            ('/image_raw', '/image_raw_low') 
+        ]
+    )
+
+    # Running Rviz2
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', os.path.join(yolo_orb3_ros2_share_dir, 'rviz', 'slam.rviz')],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        camera_node,
+        image_throttle_node,
+        yolo_orb_slam_node,
+        rviz_node
+    ])
