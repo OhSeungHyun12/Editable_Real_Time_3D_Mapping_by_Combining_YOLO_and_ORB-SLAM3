89de4c9a34d4d44f66d68bb41fed937f0748b725
Ruherpan <148195357+OhSeungHyun12@users.noreply.github.com>
2025-09-02 21:17:00 +0900

    Update yolo_orb3_rviz2.launch.py

--- ros2_ws/src/yolo_orb3_ros2/launch/yolo_orb3_rviz2.launch.py
+++ ros2_ws/src/yolo_orb3_ros2/launch/yolo_orb3_rviz2.launch.py
@@ -5,8 +5,16 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    # yolo_orb3_ros2 pakage dir
+    # Pakage dir
+    # ==============================================================================
+
+    orb_slam3_root_dir = os.getenv('ORB_SLAM3_ROOT')
+    if orb_slam3_root_dir is None:
+        raise Exception('Environment variable ORB_SLAM3_ROOT is not set!')
+    
     yolo_orb3_ros2_share_dir = get_package_share_directory('yolo_orb3_ros2')
+
+    # ==============================================================================
 
     camera_node = Node(
         package='v4l2_camera',
@@ -15,12 +23,12 @@ def generate_launch_description():
         output='screen'
     )
 
-    # Limit FPS: 15FPS
+    # Limit FPS: 30FPS
     image_throttle_node = Node(
         package='topic_tools',
         executable='throttle',
         name='image_throttle',
-        arguments=['messages', '/image_raw', '15', '/image_raw_low'],
+        arguments=['messages', '/image_raw', '30', '/image_raw_low'],
         output='screen'
     )
 
@@ -31,8 +39,8 @@ def generate_launch_description():
         name='yolo_orb_slam',
         output='screen',
         arguments=[
-            os.path.join(yolo_orb3_ros2_share_dir, 'orb_slam3', 'Vocabulary', 'ORBvoc.txt'),
-            os.path.join(yolo_orb3_ros2_share_dir, 'orb_slam3', 'Examples', 'ROS2', 'webcam.yaml')
+            os.path.join(orb_slam3_root_dir, 'Vocabulary', 'ORBvoc.txt'),
+            os.path.join(yolo_orb3_ros2_share_dir, 'config', 'webcam.yaml')
         ],
         remappings=[
             ('/image_raw', '/image_raw_low') 
