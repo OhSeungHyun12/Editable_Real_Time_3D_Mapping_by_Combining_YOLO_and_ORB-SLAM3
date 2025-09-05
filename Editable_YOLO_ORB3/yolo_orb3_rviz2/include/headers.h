26f0252c8f0c8ee5bf01d3f0fa6c6d8982c17231
Ruherpan <148195357+OhSeungHyun12@users.noreply.github.com>
2025-09-05 15:17:48 +0900

    Update headers.h

--- Editable_YOLO_ORB3/yolo_orb3_rviz2/include/headers.h
+++ Editable_YOLO_ORB3/yolo_orb3_rviz2/include/headers.h
@@ -1,35 +1,54 @@
 #ifndef HEADERS_H
 #define HEADERS_H
 
+// C++
 #include <algorithm>
 #include <atomic>
-#include <condition_variable>
 #include <chrono>
+#include <condition_variable>
 #include <fstream>
 #include <mutex>
 #include <iostream>
 #include <iomanip>
 #include <thread>
 
-#include <cv_bridge/cv_bridge.hpp>
-#include <sensor_msgs/msg/image.hpp>
+// OpenCV & Bridge
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
+#include <cv_bridge/cv_bridge.hpp>
+
+#include <sensor_msgs/msg/image.hpp>
+
+// ROS 2 Core
 #include <rclcpp/rclcpp.hpp>
-#include <sophus/se3.hpp>
 
-#include <torch/script.h>
-#include <torch/torch.h>
-#include "ViewerAR.h"
-#include "YoloDetect.h"
-#include "../../../include/System.h"
-
-// Rviz2 header //
+// TF / Geometry
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+
+// Images / PointCloud
+#include <sophus/se3.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
 #include <sensor_msgs/point_cloud2_iterator.hpp>
+
+// vision_msgs (2D/3D) 
+#include <vision_msgs/msg/object_hypothesis_with_pose.hpp>
+#include <vision_msgs/msg/bounding_box2_d.hpp>
+#include <vision_msgs/msg/detection2_d.hpp>
+#include <vision_msgs/msg/detection2_d_array.hpp>
+#include <vision_msgs/msg/bounding_box3_d.hpp>
+#include <vision_msgs/msg/detection3_d.hpp>
+#include <vision_msgs/msg/detection3_d_array.hpp>
+
+// libtorch (YOLO)
+#include <torch/script.h>
+#include <torch/torch.h>
+
+// Project local
+#include "ViewerAR.h"
+#include "YoloDetect.h"
+#include <System.h>
 
 #endif// HEADERS_H
