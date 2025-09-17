85c3f622d86eecec5d6e7484a3674ac5a2327e58
Ruherpan <148195357+OhSeungHyun12@users.noreply.github.com>
2025-09-17 22:41:00 +0900

    Update headers.h

--- Editable_YOLO_ORB3/yolo_orb3_rviz2/include/headers.h
+++ Editable_YOLO_ORB3/yolo_orb3_rviz2/include/headers.h
@@ -3,14 +3,18 @@
 
 // C++
 #include <algorithm>
+#include <array>
 #include <atomic>
 #include <chrono>
+#include <cmath>
 #include <condition_variable>
 #include <fstream>
 #include <mutex>
 #include <iostream>
 #include <iomanip>
 #include <thread>
+#include <string>
+#include <unordered_map>
 
 // OpenCV & Bridge
 #include <opencv2/core/core.hpp>
@@ -18,29 +22,30 @@
 #include <opencv2/highgui.hpp>
 #include <cv_bridge/cv_bridge.hpp>
 
-#include <sensor_msgs/msg/image.hpp>
-
 // ROS 2 Core
 #include <rclcpp/rclcpp.hpp>
 
 // TF / Geometry
 #include <geometry_msgs/msg/pose_stamped.hpp>
+#include <geometry_msgs/msg/transform_stamped.hpp>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tf2_ros/transform_broadcaster.h>
 
 // Images / PointCloud
 #include <sophus/se3.hpp>
+#include <sensor_msgs/msg/image.hpp>
 #include <sensor_msgs/msg/point_cloud2.hpp>
 #include <sensor_msgs/point_cloud2_iterator.hpp>
 
 // vision_msgs (2D/3D) 
-#include <vision_msgs/msg/object_hypothesis_with_pose.hpp>
 #include <vision_msgs/msg/bounding_box2_d.hpp>
+#include <vision_msgs/msg/bounding_box3_d.hpp>
 #include <vision_msgs/msg/detection2_d.hpp>
 #include <vision_msgs/msg/detection2_d_array.hpp>
-#include <vision_msgs/msg/bounding_box3_d.hpp>
 #include <vision_msgs/msg/detection3_d.hpp>
 #include <vision_msgs/msg/detection3_d_array.hpp>
+#include <vision_msgs/msg/object_hypothesis_with_pose.hpp>
 
 // libtorch (YOLO)
 #include <torch/script.h>
@@ -50,5 +55,22 @@
 #include "ViewerAR.h"
 #include "YoloDetect.h"
 #include <System.h>
+
+// RGB function
+inline std::array<uint8_t,3> class_color(int cid) {
+    uint32_t v = 0x9e3779b9u ^ (uint32_t)cid * 0x45d9f3bu;
+    return {
+        uint8_t(64 + ( v        &0xFF)/2),
+        uint8_t(64 + ((v >> 8 ) &0xFF)/2),
+        uint8_t(64 + ((v >>16 ) &0xFF)/2)
+    };
+}
+
+inline uint32_t pack_rgb(uint8_t r,uint8_t g,uint8_t b) {
+    uint32_t u; 
+    uint8_t bgra[4] = {b,g,r,0}; 
+    std::memcpy(&u,bgra,4); 
+    return u;
+}
 
 #endif// HEADERS_H
